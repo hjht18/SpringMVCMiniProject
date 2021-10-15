50e2103ef4af2c8053f460f3f27a72905d6b114b
soon <dudjswp@gmail.com>
2021-10-15 15:49:46 +0900

    2021-10-15 Project add Commit3

--- 윤순기/First_Project/src/main/java/com/project/mini/review/ReviewController.java
+++ 윤순기/First_Project/src/main/java/com/project/mini/review/ReviewController.java
@@ -21,7 +21,6 @@ public class ReviewController {
 	private int reviewTotal;
 	private int reviewBoxNum = 1;
 	private int reviewSet=1;
-	private String queryString;
 	
 	@RequestMapping("/insertBoard.do")
 	public void insertBoard(ReviewVO bb) {
@@ -35,35 +34,47 @@ public class ReviewController {
 //		ModelAndView mav = new ModelAndView();
 		System.out.println("####[ReviewController.getBoardList]");
 		
-		queryString = request.getQueryString();
-		System.out.println("query : "+request.getQueryString());
-		
-		System.out.println("####################### getBoardList @ reviewSet : "+reviewSet);
-		int reviewLimit = 10;
-		int pageLimit = 10;
-		int reviewCount = reviewService.boardCount();
+		int reviewLimit = 10;                               // 하나의 리뷰페이지에 표시할 리뷰 개수
+		int pageLimit = 10;                                 // 리뷰 페이지를 끊는 값 (previous, next를 적용 시킬 페이지 단위) 
+		int top;                                            // JSP로 반환해 리뷰를 보여줄 최대 값
+		int bottom;                                         // JSP로 반환해 리뷰를 보여줄 최소 값
+		int reviewCount = reviewService.boardCount();       // 레코드의 총 개수
 		Map<String, Integer> pageRange = new HashMap<String, Integer>();
 		
-		reviewTotal = reviewCount / reviewLimit;
-		if(reviewCount % reviewLimit > 0) reviewTotal += 1;
-		int lastReviewBox = reviewTotal;
+		reviewTotal = reviewCount / reviewLimit;             // reviewTotal : 레코드 개수를 pageLimit으로 나눈 값
+		if(reviewCount % reviewLimit > 0) reviewTotal += 1;  // pageLimit으로 연산 후 남은 레코드가 있을 경우 페이지를 추가한다. (추가된 페이지에 남은 레코드 출력)
+		int lastReviewBox = reviewTotal;                     // lastReviewBox : 남은 레코드를 표현할 페이지까지 추가된 페이지의 총 개수
 		
 		// reviewBox
-		reviewBox = reviewTotal / pageLimit;
+		reviewBox = reviewTotal / pageLimit;                 // reviewBox
 		if(reviewTotal % pageLimit > 0) reviewBox += 1;
 		
 		if(reviewTotal >= pageLimit) reviewTotal = pageLimit * reviewBoxNum;
 		
 		if(reviewBox == reviewBoxNum) {
-			int top = lastReviewBox;
-			int bottom = reviewBox*pageLimit-reviewLimit;
+			top = lastReviewBox;
+			if(top % 10 == 1) {
+				bottom = top;
+			} else {
+				bottom = reviewBox*pageLimit-reviewLimit;
+			}
 			if(bottom < 1) bottom = 1;
+			System.out.println("top : "+top);
+			System.out.println("bottom : "+bottom);
 			pageRange.put("top", top);
 			pageRange.put("bottom", bottom);
 			
 		} else {
-			pageRange.put("top", reviewTotal);
-			pageRange.put("bottom", reviewTotal-(pageLimit-1));
+			System.out.println("reviewTotal : "+reviewTotal);
+			System.out.println("reviewTotal-(pageLimit-1) : "+(reviewTotal-(pageLimit-1)));
+			top = reviewTotal;
+			bottom = reviewTotal-(pageLimit-1);
+			
+			if(bottom < 0) {
+				bottom = 1;
+			}
+			pageRange.put("top", top);
+			pageRange.put("bottom", bottom);
 		}
 //		mav.addObject("pageRange", pageRange);
 //		mav.addObject("boardList", reviewService.getBoardlist(page));
