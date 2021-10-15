cb99085d06e15f6b1af4184ecf37842a2cd054c3
soon <dudjswp@gmail.com>
2021-10-15 15:12:12 +0900

    empty commit

--- First_Project/src/main/java/com/root/biz/board/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.root.biz.board;
-
-import java.util.List;
-
-public interface BoardService {
-	public void insertBoard(BoardBean bb);
-	public List<BoardBean> getBoardlist(int pageNum);
-	public int boardCount();
-}
